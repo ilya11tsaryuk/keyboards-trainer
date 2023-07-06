8783eb9959c51a39da6ff3820e9ad11b34887e8d
ilya11tsaryuk <ilya11tsaryuk@gmail.com>
2023-07-06 13:16:40 +0800

    added styles

--- src/Components/KeyBoard.tsx
+++ src/Components/KeyBoard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, } from "@mui/material";
+import { Box, Button, Grid, } from "@mui/material";
 import React, { useEffect, useRef } from "react";
 import Keyboard from "react-simple-keyboard";
 import 'react-simple-keyboard/build/css/index.css';
@@ -10,14 +10,39 @@ type PropsKeyBoard = {
 
 const KeyBoard: React.FC<PropsKeyBoard> = ({ listButton }) => {
 
+    const totalColumns = 14;
+    const minButtonWidth = 100 / totalColumns;
+    const capsButtonWidth = minButtonWidth * 1.3;
+    const enterButtonWidth = minButtonWidth * 1.7;
+    const shiftButtonWidth = minButtonWidth * 2;
+    const spaceButtonWidth = minButtonWidth * 5;
+
+    const getButtonWidth = (value: string) => {
+        if (value === "CAPS") {
+            return `${capsButtonWidth}%`;
+        }
+        if (value === "ENTER") {
+            return `${enterButtonWidth}%`;
+        }
+        if (value === "SHIFT") {
+            return `${shiftButtonWidth}%`;
+        }
+        if (value === "SPACE") {
+            return `${spaceButtonWidth}%`;
+        }
+        return `${minButtonWidth}%`;
+    };
+
     return (
-        <Box sx={{ display: "grid", gridTemplateColumns: 'repeat(14, 1fr)', gap: "3px" }}>
+        <Grid justifyContent={'center'} container>
             {listButton.map((button) => (
-                <Button key={button.id} sx={{ width: "7%" }}>
-                    {button?.value}
-                </Button>
+                <Grid sx={{ width: getButtonWidth(button?.value), margin: 'auto' }} item key={button.id}>
+                    <Button size="small" key={button.id} sx={{ border: 1, width: "20px", minWidth: `90%` }}>
+                        {button?.value}
+                    </Button>
+                </Grid>
             ))}
-        </Box>
+        </Grid>
     )
 }
 
